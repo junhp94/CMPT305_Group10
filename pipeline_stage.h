60dbc339cccd56409a7d585ff12b0069cd146f99
Jun Park <pch9192@gmail.com>
2024-04-06 14:01:57 -0700

    added stage classes

--- pipeline_stage.h
+++ pipeline_stage.h
@@ -10,6 +10,59 @@ public:
     std::vector<Trace> instructions;
 
     // insert, remove implementation
+    virtual void process(const Trace &instruction) = 0;
+    virtual bool isEmpty() const = 0;
+    virtual ~PipelineStage() {}
+};
+
+class IFStage : public PipelineStage
+{
+private:
+    std::vector<Trace> instructions;
+
+public:
+    void process(const Trace &instruction) override;
+    bool isEmpty() const override;
+};
+
+class IDStage : public PipelineStage
+{
+private:
+    std::vector<Trace> instructions;
+
+public:
+    void process(const Trace &instruction) override;
+    bool isEmpty() const override;
+};
+
+class EXStage : public PipelineStage
+{
+private:
+    std::vector<Trace> instructions;
+
+public:
+    void process(const Trace &instruction) override;
+    bool isEmpty() const override;
+};
+
+class MEMStage : public PipelineStage
+{
+private:
+    std::vector<Trace> instructions;
+
+public:
+    void process(const Trace &instruction) override;
+    bool isEmpty() const override;
+};
+
+class WBStage : public PipelineStage
+{
+private:
+    std::vector<Trace> instructions;
+
+public:
+    void process(const Trace &instruction) override;
+    bool isEmpty() const override;
 };
 
 #endif
