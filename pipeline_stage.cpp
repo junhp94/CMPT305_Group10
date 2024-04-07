bb771e780b2493b7c7a64639e44088eec6942302
Jun Park <pch9192@gmail.com>
2024-04-07 15:37:19 -0700

    5 stage classes added

--- pipeline_stage.cpp
+++ pipeline_stage.cpp
@@ -1,1 +1,61 @@
 #include "pipeline_stage.h"
+
+void PipelineStage::process(const Trace &instruction)
+{
+    instructions.push_back(instruction);
+}
+
+bool PipelineStage::isEmpty() const
+{
+    return instructions.empty();
+}
+
+void IFStage::process(const Trace &instruction)
+{
+    PipelineStage::process(instruction);
+}
+
+bool IFStage::isEmpty() const
+{
+    return PipelineStage::isEmpty();
+}
+
+void IDStage::process(const Trace &instruction)
+{
+    PipelineStage::process(instruction);
+}
+
+bool IDStage::isEmpty() const
+{
+    return PipelineStage::isEmpty();
+}
+
+void EXStage::process(const Trace &instruction)
+{
+    PipelineStage::process(instruction);
+}
+
+bool EXStage::isEmpty() const
+{
+    return PipelineStage::isEmpty();
+}
+
+void MEMStage::process(const Trace &instruction)
+{
+    PipelineStage::process(instruction);
+}
+
+bool MEMStage::isEmpty() const
+{
+    return PipelineStage::isEmpty();
+}
+
+void WBStage::process(const Trace &instruction)
+{
+    PipelineStage::process(instruction);
+}
+
+bool WBStage::isEmpty() const
+{
+    return PipelineStage::isEmpty();
+}
